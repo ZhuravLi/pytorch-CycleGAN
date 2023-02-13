c6ec645e87baff26041d2d1f4d732255ba28cb4a
Lizaveta <zhuravleva.elizaveta11235@gmail.com>
2023-02-13 20:56:08 +0300

    updated link to weights file

--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -9,8 +9,8 @@ echo "Specified [$FILE]"
 
 mkdir -p ./checkpoints/${FILE}_pretrained
 ZIP_FILE=./checkpoints/${FILE}_pretrained.zip
-FILE_ID=1rToFkTXczXiTLJw3CrWodQJ487Sded7H
-URL=https://drive.google.com/uc?export=download&id=1rToFkTXczXiTLJw3CrWodQJ487Sded7H
+FILE_ID=1VrhGVyPeJYmdNMdIIp7Aip6Q64QB9tQm
+URL=https://drive.google.com/uc?export=download&id=1VrhGVyPeJYmdNMdIIp7Aip6Q64QB9tQm
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILE_ID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILE_ID" -O $ZIP_FILE && rm -rf /tmp/cookies.txt
 unzip $ZIP_FILE -d ./checkpoints/${FILE}_pretrained
